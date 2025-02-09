0a96f04ce1d661338dd51b382ebc478bc2566628
s-uhyunkim <suhyun.kim@protonmail.com>
2025-02-09 15:03:14 -0500

    Implement output template and error handling for parsing logic statements
    - Create a new output.html template that displays parsing results and errors.
    - Modify the simplify function to handle exceptions when parsing logic statements, rendering the new output template if an error occurs.
    - Remove output-related code from index.html to streamline the input process.

--- pogical.py
+++ pogical.py
@@ -18,11 +18,14 @@ async def index(request: Request):
 @app.post("/")
 # Recommended to use Annotated according to the current FastAPI docs
 async def simplify(request: Request, logic_statement_string: Annotated[str, Form()]):
-    sympy_expression = expression.parse_string(logic_statement_string)[0] # W/o [0], the var would be ParseResult object
+    try:
+        sympy_expression = expression.parse_string(logic_statement_string)[0] # W/o [0], the var would be ParseResult object
+    except:
+        return templates.TemplateResponse("output.html", {"hasException" : True})
     sympy_expression_dot = dotprint(sympy_expression) # converts sympy_expression into a DOT string
     simplification = simplify_logic(sympy_expression) # function only takes in sympy 'Boolean' input
     simplification_dot = dotprint(simplification)
-    return templates.TemplateResponse("index.html", {"request": request,
+    return templates.TemplateResponse("output.html", {"request": request,
                                                      "logic_statement_string": logic_statement_string,
                                                      "sympy_expression": sympy_expression,
                                                      "sympy_expression_dot": sympy_expression_dot,
