490c2f393912302056dee77493732a5a045eba0c
s-uhyunkim <suhyun.kim@protonmail.com>
2025-02-09 13:43:40 -0500

    Rename input variable for clarity and update corresponding template references. Changed the <p> tags to <pre>.

--- pogical.py
+++ pogical.py
@@ -18,13 +18,13 @@ async def index(request: Request):
 
 @app.post("/")
 # Recommended to use Annotated according to the current FastAPI docs
-async def simplify(request: Request, logic_statement_input: Annotated[str, Form()]):
-    sympy_expression = expression.parse_string(logic_statement_input)[0] # W/o [0], the var would be ParseResult object
+async def simplify(request: Request, logic_statement_string: Annotated[str, Form()]):
+    sympy_expression = expression.parse_string(logic_statement_string)[0] # W/o [0], the var would be ParseResult object
     sympy_expression_dot = dotprint(sympy_expression) # converts sympy_expression into a DOT string
     simplification = simplify_logic(sympy_expression) # function only takes in sympy 'Boolean' input
     simplification_dot = dotprint(simplification)
     return templates.TemplateResponse("index.html", {"request": request,
-                                                     "logic_statement_input": logic_statement_input,
+                                                     "logic_statement_string": logic_statement_string,
                                                      "sympy_expression": sympy_expression,
                                                      "sympy_expression_dot": sympy_expression_dot,
                                                      "simplification": simplification,
