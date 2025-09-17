a1d5f543326cbd405216116da8b2d992b0fe85c6
s-uhyunkim <suhyun.kim@protonmail.com>
2025-09-17 14:48:54 -0400

    Refactor grammar parsing and simplify handling of implicit conjunctions - Add `make_conjunctions_explicit()` to preprocess implicit conjunctions efficiently. - Rename constants to Pythonic styling - Add documentation in `grammar.py` and `pogical.py`. - Improve formatting in `output.html`.

--- grammar.py
+++ grammar.py
@@ -1,134 +1,161 @@
-from pyparsing import ParserElement, infix_notation, opAssoc, Suppress, one_of, Word, pyparsing_unicode
+"""
+    Provides a grammar for parsing any string containing logical expressions.
+"""
+
+import re
+from pyparsing import ParserElement, infix_notation, opAssoc, Suppress, one_of, Word
+from pyparsing.unicode import pyparsing_unicode
 from sympy import Symbol
 from sympy.logic.boolalg import true, false, Not, And, Nand, Or, Xor, Nor, Xnor, Implies, Equivalent
 
-ParserElement.enablePackrat() # Simplifies delimiter parsing and speeds up parsing in general
-
-def is_atom_start(char):
-    return char.isalpha() or char == prefix_negation or char == left_delimiter
-def is_atom_end(char):
-    return char.isalpha() or char == postfix_negation or char == right_delimiter
-
-def preprocess(input_string):
-    input_string_list = list(input_string)
-    i = 1
-    while i < len(input_string_list):
-        if is_atom_start(input_string_list[i]) and is_atom_end(input_string_list[i - 1]):
-            input_string_list.insert(i, '.')
-            i += 1  # Skip the inserted conjunction
-        i += 1
-    return ''.join(input_string_list)
+ParserElement.enable_packrat()  # since there's 6 levels of precedence in infix_notation()
 
 
+PREFIX_NEGATION_LITERALS = "~ ! - ¬ −"
+POSTFIX_NEGATION_LITERALS = "' ′ ̅"
+LEFT_DELIMITER_LITERALS = "( [ {"
+RIGHT_DELIMITER_LITERALS = ") ] }"
 
-variable = Word(pyparsing_unicode.BasicMultilingualPlane.alphas)
-# ≡ and ⟚ are reserved for equivalence, not biconditional TODO: parse!
-# ≢ is reserved for non-equivalence, not exclusive disjunction TODO: parse!
-tautology = one_of("1 T ⊤")
-contradiction = one_of("0 F ⊥")
+NON_ALNUM_LOOKBEHINDS = (POSTFIX_NEGATION_LITERALS + RIGHT_DELIMITER_LITERALS).replace(" ", "")
+NON_ALNUM_LOOKAHEADS = (PREFIX_NEGATION_LITERALS + LEFT_DELIMITER_LITERALS).replace(" ", "")
+ESCAPED_LOOKBEHINDS = re.escape(NON_ALNUM_LOOKBEHINDS)
+ESCAPED_LOOKAHEADS = re.escape(NON_ALNUM_LOOKAHEADS)
 
-prefix_negation = one_of("~ ! - ¬ −")
-postfix_negation = one_of("' ′ ̅")
-conjunction = one_of(". & * ∧ · ×")
-non_conjunction = one_of("| ↑ ⊼")
-disjunction = one_of("+ ∨ ∥")
-exclusive_disjunction = one_of("^ ⊕ ⊻")
-non_disjunction = one_of("↓ ⊽")
-exclusive_non_disjunction = one_of("⊙")
-implication = one_of("> → ⇒ ⊃")
-converse = one_of("< ← ⇐ ⊂")
-non_implication = one_of("↛ ⇏ ⊅")
-non_converse = one_of("↚ ⇍ ⊄")
-biconditional = one_of("↔ ⇔")
-non_biconditional = one_of("↮ ⇎")
+IMPLICIT_CONJUNCTION = re.compile(rf"(?<=[a-zA-Z{ESCAPED_LOOKBEHINDS}])(?=[a-zA-Z{ESCAPED_LOOKAHEADS}])")
 
-left_delimiter = one_of("( [ {").suppress()
-right_delimiter = one_of(") ] }").suppress()
+def make_conjunctions_explicit(input_string):  # since parsing all implicit conjunctions directly is inefficient
+    """Make every implicit conjunction explicit and return a new string."""
+    return IMPLICIT_CONJUNCTION.sub('.', input_string)
 
-# Parse actions
-# evaluate=False prevents Sympy from auto-simplifying expressions during parsing
+
+# TODO: parse ≡ and ⟚ as equivalences
+# TODO: parse ≢ as non-equivalence
+BMP = pyparsing_unicode.BasicMultilingualPlane
+VARIABLE = Word(BMP.identchars, BMP.identbodychars)
+
+LEFT_DELIMITER = one_of(LEFT_DELIMITER_LITERALS).suppress()
+RIGHT_DELIMITER = one_of(RIGHT_DELIMITER_LITERALS).suppress()
+
+TAUTOLOGY = one_of("1 T ⊤")
+CONTRADICTION = one_of("0 F ⊥")
+
+# TODO: include easy-to-type strings for all operators
+PREFIX_NEGATION = one_of(PREFIX_NEGATION_LITERALS)
+POSTFIX_NEGATION = one_of(POSTFIX_NEGATION_LITERALS)
+CONJUNCTION = one_of(". & * ∧ · ×")
+NON_CONJUNCTION = one_of("| ↑ ⊼")
+DISJUNCTION = one_of("+ ∨ ∥")
+EXCLUSIVE_DISJUNCTION = one_of("^ ⊕ ⊻")
+NON_DISJUNCTION = one_of("↓ ⊽")
+EXCLUSIVE_NON_DISJUNCTION = one_of("⊙")
+IMPLICATION = one_of("> → ⇒ ⊃")
+CONVERSE = one_of("< ← ⇐ ⊂")
+NON_IMPLICATION = one_of("↛ ⇏ ⊅")
+NON_CONVERSE = one_of("↚ ⇍ ⊄")
+BICONDITIONAL = one_of("↔ ⇔")
+NON_BICONDITIONAL = one_of("↮ ⇎")
+
+
+# Setting evaluate=False to display original parsed input to user
 
 def make_variable(tokens):
+    """Parse a token as a ``Symbol`` object and return it."""
     return Symbol(tokens[0])
 
+
 def make_tautology():
+    """Parse a token as ``BooleanTrue`` and return it."""
     return true
 
+
 def make_contradiction():
+    """Parse a token as ``BooleanFalse`` and return it."""
     return false
 
+
 def make_prefix_negation(tokens):
-    atom = tokens[0][1]
-    return Not(atom, evaluate=False)
+    """Parse a token as ``Not`` with parsed arguments and return it."""
+    literal = tokens[0][1]
+    return Not(literal, evaluate=False)
+
 
 def make_postfix_negation(tokens):
-    literal = tokens[0][0] # Start with the atom, but it'll be appended recursively
-    for _ in tokens[0][1:]: # Just applies Not() n times
+    """Parse a token as ``Not`` with parsed arguments and return it."""
+    literal = tokens[0][0]
+    for _ in tokens[0][1:]:
         literal = Not(literal, evaluate=False)
     return literal
 
-# Dispatch parse actions like make_conjunction parse left to right *by pairs*
-# I think parsing all AND and NAND chains at once, by sorting them and then combining them,
-# is much less efficient and readable
+
+# Having the below dispatch functions parse pair-wise is much more efficient
+# and readable than parsing a chain of args
 def make_conjunction(tokens):
+    """Parse three tokens as ``And`` or ``Nand`` with parsed arguments and return it."""
     chain = tokens[0]
     left_operand = chain[0]
-    for operator, right_operand in zip(chain[1::2], chain[2::2]): # Must iterate because tokens is a flat list
-        if operator == non_conjunction:
+    for operator, right_operand in zip(chain[1::2], chain[2::2]):  # Must iterate because tokens is a flat list
+        if operator == NON_CONJUNCTION:
             left_operand = Nand(left_operand, right_operand, evaluate=False)
         else:
             left_operand = And(left_operand, right_operand, evaluate=False)
     return left_operand
 
+
 def make_disjunction(tokens):
+    """Parse three tokens as ``Or``, ``Xor``, ``Nor``, or ``Xnor`` with parsed arguments and return it."""
     chain = tokens[0]
     left_operand = chain[0]
     for operator, right_operand in zip(chain[1::2], chain[2::2]):
-        if operator == exclusive_disjunction:
+        if operator == EXCLUSIVE_DISJUNCTION:
             left_operand = Xor(left_operand, right_operand, evaluate=False)
-        elif operator == non_disjunction:
+        elif operator == NON_DISJUNCTION:
             left_operand = Nor(left_operand, right_operand, evaluate=False)
-        elif operator == exclusive_non_disjunction:
+        elif operator == EXCLUSIVE_NON_DISJUNCTION:
             left_operand = Xnor(left_operand, right_operand, evaluate=False)
         else:
             left_operand = Or(left_operand, right_operand, evaluate=False)
     return left_operand
 
+
 def make_implication(tokens):
+    """Parse three tokens as ``Implies``, converse, non-implication, or non-converse with parsed arguments and return it."""
     chain = tokens[0]
     left_operand = chain[0]
     for operator, right_operand in zip(chain[1::2], chain[2::2]):
-        if operator == converse:
+        if operator == CONVERSE:
             left_operand = Implies(right_operand, left_operand, evaluate=False)
-        elif operator == non_implication:
+        elif operator == NON_IMPLICATION:
             left_operand = Not(Implies(left_operand, right_operand, evaluate=False))
-        elif operator == non_converse:
+        elif operator == NON_CONVERSE:
             left_operand = Not(Implies(right_operand, left_operand, evaluate=False))
         else:
             left_operand = Implies(left_operand, right_operand, evaluate=False)
     return left_operand
 
+
 def make_biconditional(tokens):
+    """Parse three tokens as ``Equivalence`` or non-biconditional with parsed arguments and return it."""
     chain = tokens[0]
     left_operand = chain[0]
     for operator, right_operand in zip(chain[1::2], chain[2::2]):
-        if operator == non_conjunction:
+        if operator == NON_CONJUNCTION:
             left_operand = Not(Equivalent(left_operand, right_operand, evaluate=False))
         else:
             left_operand = Equivalent(left_operand, right_operand, evaluate=False)
     return left_operand
 
-# expression definition
-expression = infix_notation(variable.set_parse_action(make_variable) | # See pyparsing docs for Or and MatchFirst
-                            tautology.set_parse_action(make_tautology) |
-                            contradiction.set_parse_action(make_contradiction),
-    [
-        (prefix_negation, 1, opAssoc.RIGHT, make_prefix_negation),  # \ Ordered so due to left-to-right precedence
-        (postfix_negation, 1, opAssoc.LEFT, make_postfix_negation), # /
-        (conjunction ^ non_conjunction, 2, opAssoc.LEFT, make_conjunction),
-        (disjunction ^ exclusive_disjunction ^ non_disjunction ^ exclusive_non_disjunction, 2, opAssoc.LEFT, make_disjunction),
-        (implication ^ converse ^ non_implication ^ non_converse, 2, opAssoc.LEFT, make_implication),
-        (biconditional ^ non_biconditional, 2, opAssoc.LEFT, make_biconditional),
-    ],
-    Suppress(left_delimiter), Suppress(right_delimiter)
-)
+
+# MatchFirst (|) is more efficient than Or (^) since each ParserElement contains unique chars
+expression = infix_notation(VARIABLE.set_parse_action(make_variable) |
+                            TAUTOLOGY.set_parse_action(make_tautology) |
+                            CONTRADICTION.set_parse_action(make_contradiction),
+                            [  # First two are ordered so due to left-to-right precedence
+                                (PREFIX_NEGATION, 1, opAssoc.RIGHT, make_prefix_negation),
+                                (POSTFIX_NEGATION, 1, opAssoc.LEFT, make_postfix_negation),
+                                (CONJUNCTION | NON_CONJUNCTION, 2, opAssoc.LEFT, make_conjunction),
+                                (DISJUNCTION | EXCLUSIVE_DISJUNCTION | NON_DISJUNCTION | EXCLUSIVE_NON_DISJUNCTION, 2, opAssoc.LEFT, make_disjunction),
+                                (IMPLICATION | CONVERSE | NON_IMPLICATION | NON_CONVERSE, 2, opAssoc.LEFT, make_implication),
+                                (BICONDITIONAL | NON_BICONDITIONAL, 2, opAssoc.LEFT, make_biconditional),
+                            ],
+                            Suppress(LEFT_DELIMITER), Suppress(RIGHT_DELIMITER)
+                        )
--- pogical.py
+++ pogical.py
@@ -1,3 +1,6 @@
+"""
+    TEMP
+"""
 from typing import Annotated
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
@@ -5,30 +8,29 @@ from sympy import pretty
 from sympy.logic.boolalg import simplify_logic, to_cnf, to_dnf, to_anf, to_nnf
 from sympy.printing.dot import dotprint
 
-# Logic parsing grammar; see pyparsing tutorials or docs
-from grammar import expression, preprocess
+from grammar import expression, make_conjunctions_explicit
 
+from pyeda.boolalg import espresso
+
+# Boilerplate code
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-
-
 @app.get("/")
-async def index(request: Request):
+async def read_root(request: Request):
+    """Return a ``Coroutine`` of the ``index.html`` template and the ``request`` value."""
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post("/")
 # Recommended to use Annotated according to the FastAPI docs
 # Since the checkbox is in the <form> tag, implicit_conjunctions is processed like input_string, but instead None is a valid value
 async def simplify(request: Request, input_string: Annotated[str, Form()], implicit_conjunctions: Annotated[str, Form()] = None):
-    try: # without [0], boolean_expression would be ParseResult object
-        print(implicit_conjunctions)
-        if implicit_conjunctions is not None:
-            boolean_expression = expression.parse_string(preprocess(input_string))[0]
-        else:
-            boolean_expression = expression.parse_string(input_string)[0]
-    except:
-        return templates.TemplateResponse("output.html", {"hasException" : True}) # toggles other block definition in output.html
+    # without [0], boolean_expression would be ParseResult object
+    if implicit_conjunctions is not None:
+        boolean_expression = expression.parse_string(make_conjunctions_explicit(input_string))[0]
+    else:
+        boolean_expression = expression.parse_string(input_string)[0]
     boolean_expression_dot = dotprint(boolean_expression) # converts boolean_expression into a DOT string
 
     simplified = simplify_logic(boolean_expression, deep=False, force=True) # function only takes in sympy 'Boolean' input
@@ -41,6 +43,7 @@ async def simplify(request: Request, input_string: Annotated[str, Form()], impli
     anf_simplified_dot = dotprint(anf_simplified)
     nnf_simplified = to_nnf(simplified)
     nnf_simplified_dot = dotprint(nnf_simplified)
+
 
     return templates.TemplateResponse("output.html", {"request": request,
                                                      "input_string": input_string,
